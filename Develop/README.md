6905749928e4e721fc2395cd430618847d02dd34
Tuba Muktadir <muktadir1090@gmail.com>
2024-09-21 01:08:33 -0400

    added github link, email

--- Develop/README.md
+++ Develop/README.md
@@ -1,21 +1,43 @@
 
-  # Project Title: piano
-  
-  ## Table Of Content:
-  kk,installiton, werr
-  
-  ### Description:
-  music
-  
-  ### Installation Instructions:
-  12345
-  
-  ### Usage:
-  454
-  
-  ### Contribution Guidelines:
-  33
-  
-  ### Test Instructions:
-  55
-  
+# Project Title: ss
+
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+
+## Table Of Content:
+- [Description](#description)
+- [Installation Instructions](#installation-instructions)
+- [Usage](#usage)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Test Instructions](#test-instructions)
+- [License](#license)
+- [Questions](#questions)
+- [Thanks](#thanks)
+
+### Description:
+ss
+
+### Installation Instructions:
+ss
+
+### Usage:
+sssq
+
+### Contribution Guidelines:
+qq
+
+### Test Instructions:
+11
+
+### License:
+This project is licensed under the MIT license.
+
+### Questions:
+If you have any questions, you can reach me at:
+
+- GitHub: [muktadir1090](https://github.com/muktadir1090)
+- Email: [muktadir1090@gmal.com](mailto:muktadir1090@gmal.com)
+
+Preferred method of contact: email
+
+### Thanks:
+Thank you for checking out this project! Your support and feedback are greatly appreciated.
--- Develop/index.js
+++ Develop/index.js
@@ -4,88 +4,136 @@ const fs = require('fs').promises;
 
 // Array of questions for user input
 const questions = [
-    {
-      type: 'input',
-      message: 'Write the name of your Project title?',
-      name: 'title',
-    },
-    {
-      type: 'input',
-      message: 'Write the description of your project',
-      name: 'description',
-    },
-    {
-      type: 'input',
-      message: 'Enter Table of Content',
-      name: 'table_content',
-    },
-    {
-      type: 'input',
-      message: 'Enter installation instructions',
-      name: 'installation',
-    },
-    {
-      type: 'input',
-      message: 'Enter usage information',
-      name: 'usage',
-    },
-    {
-      type: 'input',
-      message: 'Enter contribution guidelines',
-      name: 'contributing',
-    },
-    {
-      type: 'input',
-      message: 'Enter test instructions',
-      name: 'tests',
-    },
-  ];
-  
-  // Function to generate the README content
-  function generateReadMe(answers) {
-    return `
-  # Project Title: ${answers.title}
-  
-  ## Table Of Content:
-  ${answers.table_content}
-  
-  ### Description:
-  ${answers.description}
-  
-  ### Installation Instructions:
-  ${answers.installation}
-  
-  ### Usage:
-  ${answers.usage}
-  
-  ### Contribution Guidelines:
-  ${answers.contributing}
-  
-  ### Test Instructions:
-  ${answers.tests}
-  `;
-  }
-  
+  {
+    type: 'input',
+    message: 'Write the name of your Project title?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Write the description of your project',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'Enter Table of Content',
+    name: 'table_content',
+  },
+  {
+    type: 'input',
+    message: 'Enter installation instructions',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Enter usage information',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'Enter contribution guidelines',
+    name: 'contributing',
+  },
+  {
+    type: 'input',
+    message: 'Enter test instructions',
+    name: 'tests',
+  },
+  {
+    type: 'list',
+    message: 'Choose a license for your project',
+    name: 'license',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    message: 'Enter your GitHub username (or link)',
+    name: 'github',
+  },
+  {
+    type: 'input',
+    message: 'Enter your email address',
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: 'How should users contact you (GitHub, email, etc.)?',
+    name: 'contact_method',
+  },
+];
 
-// TODO: Create a function to write README file
+// Function to generate license badge URL
+function getLicenseBadge(license) {
+  if (license === 'None') return '';
+  return `![License](https://img.shields.io/badge/License-${license.replace(/\s+/g, '%20')}-blue.svg)`;
+}
+
+// Function to generate the README content
+function generateReadMe(answers) {
+  const licenseBadge = getLicenseBadge(answers.license);
+  return `
+# Project Title: ${answers.title}
+
+${licenseBadge}
+
+## Table Of Content:
+- [Description](#description)
+- [Installation Instructions](#installation-instructions)
+- [Usage](#usage)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Test Instructions](#test-instructions)
+- [License](#license)
+- [Questions](#questions)
+- [Thanks](#thanks)
+
+### Description:
+${answers.description}
+
+### Installation Instructions:
+${answers.installation}
+
+### Usage:
+${answers.usage}
+
+### Contribution Guidelines:
+${answers.contributing}
+
+### Test Instructions:
+${answers.tests}
+
+### License:
+This project is licensed under the ${answers.license} license.
+
+### Questions:
+If you have any questions, you can reach me at:
+
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: [${answers.email}](mailto:${answers.email})
+
+Preferred method of contact: ${answers.contact_method}
+
+### Thanks:
+Thank you for checking out this project! Your support and feedback are greatly appreciated.
+`;
+}
+
+// Function to write README file
 async function writeToFile(fileName, data) {
-    try {
-      await fs.writeFile(fileName, data);
-      console.log('Successfully created README.MD!');
-    } catch (err) {
-      console.log(err);
-    }
+  try {
+    await fs.writeFile(fileName, data);
+    console.log('Successfully created README.MD!');
+  } catch (err) {
+    console.log(err);
   }
+}
 
-
-
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-      const readMePageContent = generateReadMe(answers);
-      writeToFile('README.md', readMePageContent);
-    });
-  }
+  inquirer.prompt(questions).then((answers) => {
+    const readMePageContent = generateReadMe(answers);
+    writeToFile('README.md', readMePageContent);
+  });
+}
 
 // Function call to initialize app
 init();
